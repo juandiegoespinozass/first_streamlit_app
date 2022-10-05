2f1d2b69feb40eb62f882193802e15349cb67768
JUAN DIEGO ESPINOZA SUAREZ <115045375+juandiegoespinozass@users.noreply.github.com>
2022-10-05 09:01:29 -0600

    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,10 +8,16 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg') 
 streamlit.text('🥑🍞 Avocado Toast') 
 
+streamlit.text('Juan Diego Test') 
+
+
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothiee 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt") 
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list) 
+
+
+
  
