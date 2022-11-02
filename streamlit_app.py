411bcc740de9fe1c75f035c6c86baf564cb01dee
JUAN DIEGO ESPINOZA SUAREZ <115045375+juandiegoespinozass@users.noreply.github.com>
2022-11-02 11:53:18 -0600

    requests new

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit
 import pandas
+import requests
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
 
 streamlit.title('My parents New Healthy Diner')
 streamlit.header('Breakfast menu')
@@ -16,6 +20,7 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show) 
+streamlit.text(fruityvice_response)
 
 
 
